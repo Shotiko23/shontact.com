d0f1db9ff65812d65de1650e0af8865b97e0a49b
Shotiko23 <156598714+Shotiko23@users.noreply.github.com>
2024-01-14 20:09:58 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function toggleCompanyDetails(card) {
+    const details = card.querySelector('.company-details');
+    details.style.display = details.style.display === 'none' ? 'block' : 'none';
+}
